791cd8f9f5dce4519cafdddda4610648ca0ac97e
bpiper91 <bpiper91@gmail.com>
2022-06-30 13:50:52 -0400

    add js folder and some pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// when I open the planner, display the current day at the top of the calendar
+    // format Day, Month #th
+
+// when I scroll down, show time blocks for standard business hours
+    // 9am through 5pm
+
+// when I view the time blocks for the day, time blocks are color coded
+    // past is gray, present is red, future is green
+
+// when I click on a time block, I can type to enter an event/meeting
+    // when I click the save button on the right (noted with an icon), save text in localStorage
+
+// when I refresh the page, save text persists
